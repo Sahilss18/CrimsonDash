e0b7db73523992931a1e21eb1d308c6182695a86
Sahil Singh <Sahilss18@users.noreply.github.com>
2026-08-30 16:36:41 +0530

    feat(world): implement procedural infinite terrain generator and parallax backgrounds

---
+++ Assets/Scripts/CrimsonDash/TerrainGeneration/DefaultBlock.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CrimsonDash.TerrainGeneration
+{
+
+	public class DefaultBlock : Block
+	{
+
+	}
+
+}
---
+++ Assets/Scripts/CrimsonDash/TerrainGeneration/DefaultTerrainGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using CrimsonDash.Characters;
+
+namespace CrimsonDash.TerrainGeneration
+{
+
+	public class DefaultTerrainGenerator : TerrainGenerator
+	{
+
+	}
+
+}

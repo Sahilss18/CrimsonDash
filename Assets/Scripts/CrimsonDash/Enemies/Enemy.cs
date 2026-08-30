2b6aa44308be4833435eb83c41d075fd6d40abc1
Sahil Singh <Sahilss18@users.noreply.github.com>
2026-08-30 16:36:41 +0530

    feat(gameplay): implement player mechanics, collectibles, enemy hazards, and object pooling

---
+++ Assets/Scripts/CrimsonDash/Enemies/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using CrimsonDash.Characters;
+
+namespace CrimsonDash.Enemies
+{
+
+	public abstract class Enemy : MonoBehaviour
+	{
+
+		public abstract Collider2D Collider2D { get; }
+
+		public abstract void Kill ( Character target );
+
+	}
+
+}

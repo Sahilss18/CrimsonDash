6db73f4e1cdb3ae8589b61c5b94ea87fe6129654
Sahil Singh <Sahilss18@users.noreply.github.com>
2026-08-30 16:36:41 +0530

    feat(ui): implement HUD score tracking, screen navigations, and pause menus

---
+++ Assets/Scripts/CrimsonDash/UI/UIScreen/LoadingScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CrimsonDash.UI
+{
+    public class LoadingScreen : UIScreen
+    {
+        public override void UpdateScreenStatus(bool open)
+        {
+            base.UpdateScreenStatus(open);
+        }
+    }
+
+}

f51a0bcc237be1d13f9a3a4572a8f19dd54746ab
Sahil Singh <Sahilss18@users.noreply.github.com>
2026-08-30 16:36:41 +0530

    feat(orchestration): implement GameManager, AudioManager, and assemble game scenes

---
+++ Assets/Editor/RedRunner/TerrainGeneration/BlocksInformationGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace CrimsonDash.TerrainGeneration
+{
+
+	public class BlocksInformationGenerator : ScriptableWizard
+	{
+
+		protected override bool DrawWizardGUI ()
+		{
+			return base.DrawWizardGUI ();
+		}
+
+	}
+
+}
